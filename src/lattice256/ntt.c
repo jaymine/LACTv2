90da4f6fa2661ec14f93bd20afd535f8dc26fc96
Jayamine Alupotha <jayamine.alupotha@gmail.com>
2022-07-02 13:19:14 +1000

    Update ntt.c

--- src/lattice256/ntt.c
+++ src/lattice256/ntt.c
@@ -1,20 +1,9 @@
-/****************************************************************************
-*    LACT+ - Post-Quantum Lattice-based Aggregable Transactions (Version 2) *
-*    Copyright (C) 2022  Jayamine Alupotha                                  *
-*                                                                           *
-*    This program is free software: you can redistribute it and/or modify   *
-*    it under the terms of the GNU General Public License as published by   *
-*    the Free Software Foundation, either version 3 of the License, or      *
-*    (at your option) any later version.                                    *
-*                                                                           *
-*    This program is distributed in the hope that it will be useful,        *
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of         *
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
-*    GNU General Public License for more details.                           *
-*                                                                           *
-*    You should have received a copy of the GNU General Public License      *
-*    along with this program.  If not, see <https://www.gnu.org/licenses/>. *
-*****************************************************************************/
+/******
+ *  Based on the public domain implementation in
+ *  https://github.com/pq-crystals/dilithium
+ *  We acknowledge the authors of Dilithium.
+ *******/
+
 
 #ifndef LACTx_NTT_IMPL_H
 #define LACTx_NTT_IMPL_H
